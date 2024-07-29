2a87915418d93674613f02e46639fdaa3bb93dc4
h0ch1 <h0ch1@naver.com>
2024-07-29 15:08:21 +0900

    form module, service 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FormController } from './form/form.controller';
+import { FormService } from './form/form.service';
+import { FormModule } from './form/form.module';
 
 @Module({
-  imports: [],
+  imports: [FormModule],
   controllers: [AppController, FormController],
-  providers: [AppService],
+  providers: [AppService, FormService],
 })
 export class AppModule {}
