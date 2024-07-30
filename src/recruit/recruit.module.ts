d0a01a31780b96baea901e73008054b7a9c8f347
Seungwan Yoo <ysw99@kakao.com>
2024-07-31 01:03:21 +0900

    부원 모집 API 구현
    * ValidationPipe 추가
    - 입력 데이터 유효성 검사
    
    * 모집 스키마 생성, DB 연결
    - MongoDB 연결
    - Recruit 스키마의 applyId(지원 번호) AutoIncrement 구현
    
    * 부원 모집 기능 DB 연동
    - 중복 지원자 (학번 기준) 예외 처리 구현
    - 지원자 전체 조회 메소드 구현
    
    * DB 연결 설정 관련 수정

--- src/recruit/recruit.module.ts
+++ src/recruit/recruit.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { RecruitController } from './recruit.controller';
 import { RecruitService } from './recruit.service';
+import { Recruit, RecruitSchema } from './recruit.schema';
+import { getConnectionToken } from '@nestjs/mongoose';
+import * as AutoIncrementFactory from 'mongoose-sequence';
 
 @Module({
+  imports: [
+    MongooseModule.forFeatureAsync([
+      {
+        name: Recruit.name,
+        useFactory: async (connection) => {
+          const schema = RecruitSchema;
+          const AutoIncrement = AutoIncrementFactory(connection);
+          schema.plugin(AutoIncrement, { inc_field: 'applyId' });
+          return schema;
+        },
+        inject: [getConnectionToken()],
+      },
+    ]),
+  ],
   controllers: [RecruitController],
-  providers: [RecruitService]
+  providers: [RecruitService],
+  exports: [RecruitService],
 })
 export class RecruitModule {}
---
+++ src/recruit/recruit.schema.ts
@@ -0,0 +1,35 @@
+import { DepartmentType } from "./dto/department.type";
+import { HydratedDocument } from "mongoose";
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+export type RecruitDocument = HydratedDocument<Recruit>;
+
+@Schema()
+export class Recruit {
+
+  @Prop({ unique: true })
+  applyId: number;
+
+  @Prop({ unique: true })
+  studentId: number;
+
+  @Prop({ required: true })
+  applicantName: string;
+
+  @Prop({ required: true })
+  applicantEmail: string;
+
+  @Prop({ required: true })
+  applicantContact: string;
+
+  @Prop({ required: true })
+  applicantDept: DepartmentType;
+
+  @Prop({ required: true })
+  applicantGrade: number;
+
+  @Prop({ required: true })
+  reasonForApply: string;
+}
+
+export const RecruitSchema = SchemaFactory.createForClass(Recruit)
