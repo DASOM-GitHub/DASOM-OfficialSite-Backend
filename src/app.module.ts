b79920c0e4f2bec6b86a731cd833e00c9557b38a
Seungwan Yoo <ysw99@kakao.com>
2024-07-31 01:03:21 +0900

    부원 모집 API 구현
    * ValidationPipe 추가
    - 입력 데이터 유효성 검사
    
    * 모집 스키마 생성, DB 연결
    - MongoDB 연결
    - Recruit 스키마의 applyId(지원 번호) AutoIncrement 구현
    
    * 부원 모집 기능 DB 연동
    - 중복 지원자 (학번 기준) 예외 처리 구현
    - 지원자 전체 조회 메소드 구현
    
    * DB 연결 설정 관련 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
 import { RecruitModule } from './recruit/recruit.module';
+import { MongooseModule } from "@nestjs/mongoose";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import dbConfig from "./db.config";
 
 @Module({
-  imports: [RecruitModule]
+  imports: [
+    ConfigModule.forRoot({
+      load: [dbConfig],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('uri'),
+        serverSelectionTimeoutMS: 5000,
+        socketTimeoutMS: 45000,
+      }),
+      inject: [ConfigService],
+    }),
+    RecruitModule,
+  ]
 })
 export class AppModule {}
--- src/recruit/recruit.controller.ts
+++ src/recruit/recruit.controller.ts
@@ -1,14 +1,20 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
 import { RecruitService } from "./recruit.service";
 import { ApplyDto } from "./dto/apply.dto";
+import { Recruit } from "./recruit.schema";
 
 @Controller('recruit')
 export class RecruitController {
   constructor(private recruitService: RecruitService) {}
 
   @Post()
-  apply(@Body() applyDto: ApplyDto): ApplyDto {
+  apply(@Body() applyDto: ApplyDto): Promise<Recruit> {
     return this.recruitService.apply(applyDto);
+  }
+
+  @Get()
+  findAll(): Promise<Recruit[]> {
+    return this.recruitService.findAll();
   }
 
 }
