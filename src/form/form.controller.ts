2641d96608922824ce5be60fe8dc5e89e3f7b568
Seungwan Yoo <ysw99@kakao.com>
2024-07-31 22:21:21 +0900

    Update form.controller.ts

--- src/form/form.controller.ts
+++ src/form/form.controller.ts
@@ -11,23 +11,4 @@ export class FormController {
         return this.formService.create(createFormDto);
     }
 
-    @Get()
-    findAll() {
-        return this.formService.findAll();
-    }
-
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        return this.formService.findOne(id);
-    }
-    
-    @Patch(':id')
-    update(@Param('id') id: string, @Body() updateFormDto: CreateFormDto) {
-        return this.formService.update(id, updateFormDto);
-    }
-
-    @Delete(':id')
-    remove(@Param('id') id: string) {
-        return this.formService.remove(id);
-    }
 }
