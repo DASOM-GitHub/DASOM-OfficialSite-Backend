2fb0829f8235bc0599491b9575e7939aea2bd44f
Seungwan Yoo <ysw99@kakao.com>
2024-07-31 22:53:22 +0900

    개발 요구사항 정의 - recruit, form

--- src/form/form.controller.ts
+++ src/form/form.controller.ts
@@ -5,10 +5,23 @@ import { FormService } from './form.service';
 @Controller('form')
 export class FormController {
     constructor(private readonly formService: FormService) {}
-    
+
+    // create : 새로운 Form 생성
     @Post()
     create(@Body() createFormDto: CreateFormDto) {
         return this.formService.create(createFormDto);
     }
+
+    // findAll : 모든 Form 조회
+
+
+    // findOne : id를 기준으로 특정 Form 조회
+
+
+    // update : id를 기준으로 특정 Form 수정
+
+
+    // remove : id를 기준으로 특정 Form 삭제
+
 
 }
--- src/recruit/recruit.controller.ts
+++ src/recruit/recruit.controller.ts
@@ -7,14 +7,22 @@ import { Recruit } from "./recruit.schema";
 export class RecruitController {
   constructor(private recruitService: RecruitService) {}
 
+  // apply : 신규 지원자 등록
   @Post()
   apply(@Body() applyDto: ApplyDto): Promise<Recruit> {
     return this.recruitService.apply(applyDto);
   }
 
-  @Get()
-  findAll(): Promise<Recruit[]> {
-    return this.recruitService.findAll();
-  }
+  // findAll : 모든 지원자 조회
+
+
+  // findOne : id를 기준으로 특정 지원자 조회
+
+
+  // update : id를 기준으로 특정 지원자 수정
+
+
+  // remove : id를 기준으로 특정 지원자 삭제
+
 
 }
