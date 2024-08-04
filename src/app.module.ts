d0e709f1979d7954e0524a044d78fbead69486e5
Seungwan Yoo <ysw99@kakao.com>
2024-08-05 01:02:47 +0900

    credentials 인젝션 방식 수정 - config을 이용해 .yml 파일에서 받아오도록 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,24 +2,12 @@ import { Module } from '@nestjs/common';
 import { RecruitModule } from './recruit/recruit.module';
 import { MongooseModule } from "@nestjs/mongoose";
 import { FormModule } from './form/form.module';
-import { ConfigModule, ConfigService } from "@nestjs/config";
 import { ServiceSettingsModule } from './service-settings/service-settings.module';
-import dbConfig from "./db.config";
+import * as config from 'config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      load: [dbConfig],
-    }),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>('uri'),
-        serverSelectionTimeoutMS: 5000,
-        socketTimeoutMS: 45000,
-      }),
-      inject: [ConfigService],
-    }),
+    MongooseModule.forRoot(config.get('db').uri),
     RecruitModule,
     FormModule,
     ServiceSettingsModule,
