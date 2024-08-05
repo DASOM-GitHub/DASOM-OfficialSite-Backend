be1be5d912fbf0a9620bf4fca722dd6d0cb7cbaf
Seungwan Yoo <ysw99@kakao.com>
2024-08-05 11:22:35 +0900

    배포 환경 구성
    * credentials 인젝션 방식 수정
    - config을 이용해 .yml 파일에서 받아오도록 수정
    
    * credentials 인젝션 방식 수정2
    - cross-env와 @nestjs/config을 이용해 .env 파일로부터 인젝션하도록 수정
    
    * 배포 환경 구성
    - Actions workflow 작성
    - Dockerfile 작성

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,23 +2,23 @@ import { Module } from '@nestjs/common';
 import { RecruitModule } from './recruit/recruit.module';
 import { MongooseModule } from "@nestjs/mongoose";
 import { FormModule } from './form/form.module';
-import { ConfigModule, ConfigService } from "@nestjs/config";
 import { ServiceSettingsModule } from './service-settings/service-settings.module';
-import dbConfig from "./db.config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [dbConfig],
+      isGlobal: true,
+      envFilePath: [
+        process.env.NODE_ENV === 'prod' ?
+          'env/.env.prod' : 'env/.env.dev',
+      ],
     }),
     MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>('uri'),
-        serverSelectionTimeoutMS: 5000,
-        socketTimeoutMS: 45000,
-      }),
       inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('DB_URI'),
+      }),
     }),
     RecruitModule,
     FormModule,
