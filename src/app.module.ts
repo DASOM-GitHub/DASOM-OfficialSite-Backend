8761c2b36a20fb9614dd64731ff63c84ac161d27
Seungwan Yoo <ysw99@kakao.com>
2024-08-05 10:49:50 +0900

    credentials 인젝션 방식 수정 #2 - cross-env와 @nestjs/config을 이용해 .env 파일로부터 인젝션하도록 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,23 @@ import { RecruitModule } from './recruit/recruit.module';
 import { MongooseModule } from "@nestjs/mongoose";
 import { FormModule } from './form/form.module';
 import { ServiceSettingsModule } from './service-settings/service-settings.module';
-import * as config from 'config';
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
   imports: [
-    MongooseModule.forRoot(config.get('db').uri),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [
+        process.env.NODE_ENV === 'prod' ?
+          'env/.env.prod' : 'env/.env.dev',
+      ],
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('DB_URI'),
+      }),
+    }),
     RecruitModule,
     FormModule,
     ServiceSettingsModule,
