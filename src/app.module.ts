260ffcbf839f98efc5b2ee7aa338deb159fb0efd
JunYoung Choi <juny0955@gmail.com>
2024-08-16 14:15:33 +0900

    회원가입, 로그인 구현
    * jwt 기본 설정
    * 회원가입, jwt발금 구현
    * env 오류 해결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { RecruitModule } from './recruit/recruit.module';
-import { MongooseModule } from "@nestjs/mongoose";
 import { FormModule } from './form/form.module';
 import { ServiceSettingsModule } from './service-settings/service-settings.module';
-import { ConfigModule, ConfigService } from "@nestjs/config";
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: [
-        process.env.NODE_ENV === 'prod' ?
-          'env/.env.prod' : 'env/.env.dev',
+        process.env.NODE_ENV === 'prod' ? 'env/.env.prod' : 'env/.env.dev',
       ],
     }),
     MongooseModule.forRootAsync({
@@ -23,6 +24,8 @@ import { ConfigModule, ConfigService } from "@nestjs/config";
     RecruitModule,
     FormModule,
     ServiceSettingsModule,
+    AuthModule,
+    UserModule,
   ],
 })
 export class AppModule {}
