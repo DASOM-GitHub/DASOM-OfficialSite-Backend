ba0faf44922b7324bf5d49013a2d869f821772f1
Seungwan Yoo <ysw99@kakao.com>
2024-08-19 17:58:33 +0900

    서비스 설정 관련 세부 구현 - enum 타입으로 서비스 설정 타입 정의 - 서비스 설정 관련 엔드포인트의 접근 권한 설정

--- src/service-settings/service-settings.controller.ts
+++ src/service-settings/service-settings.controller.ts
@@ -1,31 +1,36 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from "@nestjs/common";
 import { ServiceSettingsService } from "./service-settings.service";
 import { ServiceSettingsDto } from "./dto/service-settings.dto";
 import { ServiceSettings } from "./schema/service-settings.schema";
+import { LocalAuthGuard } from "../auth/local/local.guard";
 
 @Controller('api/service')
 export class ServiceSettingsController {
   constructor(private serviceSettingsService: ServiceSettingsService) {}
 
   // configureSetting : 설정 추가
+  @UseGuards(LocalAuthGuard)
   @Post()
   configureSetting(@Body() recruitSettingsDto: ServiceSettingsDto): Promise<ServiceSettings> {
     return this.serviceSettingsService.configureSetting(recruitSettingsDto);
   }
 
   // findAll : 모든 설정 조회
+  @UseGuards(LocalAuthGuard)
   @Get()
   findAll(): Promise<ServiceSettings[]> {
     return this.serviceSettingsService.findAll();
   }
 
   // findByKey : 특정 설정 조회
+  @UseGuards(LocalAuthGuard)
   @Get(':key')
   findByKey(@Param('key') key: string): Promise<ServiceSettings> {
     return this.serviceSettingsService.findByKey(key);
   }
 
   // updateSetting : 특정 설정 수정
+  @UseGuards(LocalAuthGuard)
   @Patch(':key')
   updateSetting(@Param('key') key: string,
                 @Body() updateDto: ServiceSettingsDto)
@@ -34,6 +39,7 @@ export class ServiceSettingsController {
   }
 
   // removeSetting : 특정 설정 삭제
+  @UseGuards(LocalAuthGuard)
   @Delete(':key')
   removeSetting(@Param('key') key: string): Promise<object> {
     return this.serviceSettingsService.removeSetting(key);
