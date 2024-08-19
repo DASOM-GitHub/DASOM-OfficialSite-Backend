f993e5e0e3ddb953f2927a1b527baa64a16b171f
Seungwan Yoo <ysw99@kakao.com>
2024-08-19 11:35:32 +0200

    서비스 설정 관련 세부 구현
    - enum 타입으로 서비스 설정 타입 정의
    - 서비스 설정 관련 엔드포인트의 접근 권한 설정

--- src/service-settings/schema/service-settings.schema.ts
+++ src/service-settings/schema/service-settings.schema.ts
@@ -1,13 +1,14 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { HydratedDocument } from "mongoose";
+import { SettingsEnum } from "../dto/settings.enum";
 
 export type ServiceSettingsDocument = HydratedDocument<ServiceSettings>;
 
 @Schema()
 export class ServiceSettings {
 
-  @Prop({ required: true, unique: true })
-  key: string;
+  @Prop({ required: true, unique: true, enum: SettingsEnum })
+  key: SettingsEnum;
 
   @Prop({ required: true })
   value: string;
