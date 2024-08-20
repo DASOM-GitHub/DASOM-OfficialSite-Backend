278a3225a19897d5500d77e1888fe36dcf9b0d93
Seungwan Yoo <ysw99@kakao.com>
2024-08-20 17:05:40 +0200

    서비스 설정 엔드포인트 권한 관련 구현 수정

--- src/service-settings/service-settings.controller.ts
+++ src/service-settings/service-settings.controller.ts
@@ -2,35 +2,35 @@ import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from "@n
 import { ServiceSettingsService } from "./service-settings.service";
 import { ServiceSettingsDto } from "./dto/service-settings.dto";
 import { ServiceSettings } from "./schema/service-settings.schema";
-import { LocalAuthGuard } from "../auth/local/local.guard";
+import { JwtAuthGuard } from "../auth/jwt/jwt.guard";
 
 @Controller('api/service')
 export class ServiceSettingsController {
   constructor(private serviceSettingsService: ServiceSettingsService) {}
 
   // configureSetting : 설정 추가
-  @UseGuards(LocalAuthGuard)
+  @UseGuards(new JwtAuthGuard())
   @Post()
   configureSetting(@Body() recruitSettingsDto: ServiceSettingsDto): Promise<ServiceSettings> {
     return this.serviceSettingsService.configureSetting(recruitSettingsDto);
   }
 
   // findAll : 모든 설정 조회
-  @UseGuards(LocalAuthGuard)
+  @UseGuards(new JwtAuthGuard())
   @Get()
   findAll(): Promise<ServiceSettings[]> {
     return this.serviceSettingsService.findAll();
   }
 
   // findByKey : 특정 설정 조회
-  @UseGuards(LocalAuthGuard)
+  @UseGuards(new JwtAuthGuard())
   @Get(':key')
   findByKey(@Param('key') key: string): Promise<ServiceSettings> {
     return this.serviceSettingsService.findByKey(key);
   }
 
   // updateSetting : 특정 설정 수정
-  @UseGuards(LocalAuthGuard)
+  @UseGuards(new JwtAuthGuard())
   @Patch(':key')
   updateSetting(@Param('key') key: string,
                 @Body() updateDto: ServiceSettingsDto)
@@ -39,7 +39,7 @@ export class ServiceSettingsController {
   }
 
   // removeSetting : 특정 설정 삭제
-  @UseGuards(LocalAuthGuard)
+  @UseGuards(new JwtAuthGuard())
   @Delete(':key')
   removeSetting(@Param('key') key: string): Promise<object> {
     return this.serviceSettingsService.removeSetting(key);
