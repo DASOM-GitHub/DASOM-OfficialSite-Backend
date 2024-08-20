4672ecdc1a2710a7ec89cf10888a56e0ff61cea6
Seungwan Yoo <ysw99@kakao.com>
2024-08-20 16:30:07 +0200

    부원 모집 기능 추가 및 수정
    * 부원 모집 기능 추가 및 수정
    - 학번 중복 검사 삭제
    - CRUD 구현
    - Dept 타입을 enum으로 정의

--- src/recruit/recruit.controller.ts
+++ src/recruit/recruit.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from "@nestjs/common";
 import { RecruitService } from "./recruit.service";
 import { ApplyDto } from "./dto/apply.dto";
 import { Recruit } from "./recruit.schema";
+import { JwtAuthGuard } from "../auth/jwt/jwt.guard";
 
 @Controller('recruit')
 export class RecruitController {
@@ -14,18 +15,32 @@ export class RecruitController {
   }
 
   // findAll : 모든 지원자 조회
+  @UseGuards(new JwtAuthGuard('access_token'))
   @Get()
   findAll(): Promise<Recruit[]> {
     return this.recruitService.findAll();
   }
 
   // findOne : id를 기준으로 특정 지원자 조회
-
+  @UseGuards(new JwtAuthGuard('access_token'))
+  @Get(':applyId')
+  findOne(@Param('applyId') id: number): Promise<Recruit> {
+    return this.recruitService.findOne(id);
+  }
 
   // update : id를 기준으로 특정 지원자 수정
+  @UseGuards(new JwtAuthGuard('access_token'))
+  @Patch(':id')
+  update(@Param('id') id: number, @Body() updateDto: ApplyDto): Promise<Recruit> {
+    return this.recruitService.update(id, updateDto);
+  }
 
 
   // remove : id를 기준으로 특정 지원자 삭제
-
+  @UseGuards(new JwtAuthGuard('access_token'))
+  @Delete(':id')
+  remove(@Param('id') id: number): Promise<number> {
+    return this.recruitService.remove(id);
+  }
 
 }
