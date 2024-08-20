8ae3cb936142d799b1792b8cbcd75d3f2f1cf23e
JunYoung Choi <juny0955@gmail.com>
2024-08-20 13:15:50 +0900

    form schema id type 변경 (#19)

--- src/form/form.module.ts
+++ src/form/form.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { FormSchema } from './schema/form.schema';
+import { getConnectionToken, MongooseModule } from '@nestjs/mongoose';
+import { Form, FormSchema } from './schema/form.schema';
 import { FormController } from './form.controller';
 import { FormService } from './form.service';
+import * as AutoIncrementFactory from 'mongoose-sequence';
+
 
 @Module({
-    imports: [MongooseModule.forFeature([{ name: 'Form', schema: FormSchema }])],
+    imports: [MongooseModule.forFeatureAsync([
+            {
+            name: Form.name,
+            useFactory: async (connection) => {
+                const schema = FormSchema;
+                const AutoIncrement = AutoIncrementFactory(connection);
+                schema.plugin(AutoIncrement, { inc_field: 'formId' });
+                return schema;
+            },
+            inject: [getConnectionToken()],
+            },
+        ]),
+    ],
     controllers: [FormController],
     providers: [FormService],
 })
