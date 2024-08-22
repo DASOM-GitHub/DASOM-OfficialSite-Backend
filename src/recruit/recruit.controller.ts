4c631a7a76463a861765b03732a5a8a4008f6029
h0ch1 <h0ch1@naver.com>
2024-08-22 18:57:12 +0900

    POST -> PATCH

--- src/recruit/recruit.controller.ts
+++ src/recruit/recruit.controller.ts
@@ -45,14 +45,14 @@ export class RecruitController {
 
   // firstPass : id를 기준으로 특정 지원자 1차 합불 처리
   @UseGuards(new JwtAuthGuard('access_token'))
-  @Post('firstPass/:id')
+  @Patch('firstPass/:id')
   firstPass(@Param('id') id: number, @Body('isPass') pass: boolean): Promise<Recruit> {
     return this.recruitService.firstPass(id, pass);
   }
 
   // secondPass : id를 기준으로 특정 지원자 2차 합불 처리
   @UseGuards(new JwtAuthGuard('access_token'))
-  @Post('secondPass/:id')
+  @Patch('secondPass/:id')
   secondPass(@Param('id') id: number, @Body('isPass') pass: boolean): Promise<Recruit> {
     return this.recruitService.secondPass(id, pass);
   }
