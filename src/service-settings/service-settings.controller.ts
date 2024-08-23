11b0e02703b29e9ef28f5fbe6e2c90835edee124
Seungwan Yoo <ysw99@kakao.com>
2024-08-23 08:48:21 +0200

    서비스 설정 엔드포인트 롤백

--- src/service-settings/service-settings.controller.ts
+++ src/service-settings/service-settings.controller.ts
@@ -4,7 +4,7 @@ import { ServiceSettingsDto } from "./dto/service-settings.dto";
 import { ServiceSettings } from "./schema/service-settings.schema";
 import { JwtAuthGuard } from "../auth/jwt/jwt.guard";
 
-@Controller('service')
+@Controller('api/service')
 export class ServiceSettingsController {
   constructor(private serviceSettingsService: ServiceSettingsService) {}
 
